f6bfc1ce4283c589bb818614bf4f403c6530c0ee
rafiahmedrifatt <rafiahmed0471@gmail.com>
2025-05-14 23:18:11 +0600

    mongodb added

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
+require('dotenv').config()
 
 app.use(cors());
 app.use(express.json());
 
 
-app.get('/', (req, res) => {
-    res.send('coffee server is getting warmer')
-})
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.9lzg8ht.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+        app.get('/', (req, res) => {
+            res.send('coffee server is getting warmer')
+        })
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+
+
 
 app.listen(port, () => {
     console.log('coffee server is running on port', port)
